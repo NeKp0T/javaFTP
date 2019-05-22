d68f41b21b8e51221818767d013f84dd2f5b85d6
Maria Tigina <tigmah@mail.ru>
2019-05-22 17:36:46 +0300

    fixed server implementation

--- src/main/java/com/example/ftp/client/Server.java
+++ src/main/java/com/example/ftp/client/Server.java
@@ -1,15 +1,16 @@
 package com.example.ftp.client;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.nio.channels.spi.SelectorProvider;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -20,7 +21,7 @@ public class Server {
     /**
      * Port
      */
-    private int port;
+    private static final int PORT = 2599;
 
     /**
      * Host
@@ -34,7 +35,7 @@ public class Server {
 
     private Selector selector;
 
-    private void startServer() throws IOException {
+    public void startServer() throws IOException {
         selector = Selector.open();
         ServerSocketChannel serverChannel = ServerSocketChannel.open();
         serverChannel.configureBlocking(false);
@@ -55,7 +56,6 @@ public class Server {
             if (!key.isValid()) {
                 continue;
             }
-
             if (key.isAcceptable()) { // Accept client connections
                 accept(key);
             } else if (key.isReadable()) { // Read from client
@@ -74,21 +74,67 @@ public class Server {
     }
 
     private void read(SelectionKey key) throws IOException {
+        Charset charset = Charset.forName("ISO-8859-1");
+        CharsetDecoder decoder = charset.newDecoder();
         SocketChannel channel = (SocketChannel) key.channel();
         ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-        int numRead = -1;
-        numRead = channel.read(buffer);
 
-        if (numRead == -1) {
-            Socket socket = channel.socket();
-            System.out.println("Connection closed by client: " + socket.getRemoteSocketAddress());
-            channel.close();
-            key.cancel();
-            return;
+        while (!decoder.decode(buffer).toString().contains("#")) {
+            channel.read(buffer);
+        }
+        channel.close();
+        key.cancel();
+
+        buffer.flip();
+        String request = decoder.decode(buffer).toString();
+        System.out.println("request: " + request);
+
+        int requestType = request.charAt(0);
+        switch (requestType) {
+            case 1:
+                listRequest(path);
+                break;
+            case 2:
+                getRequest(path);
         }
 
-        byte[] data = new byte[numRead];
-        System.arraycopy(buffer.array(), 0, data, 0, numRead);
-        System.out.println("Got: " + new String(data));
+        buffer.clear();
+    }
+
+    File dir1 = new File("C://SomeDir");
+
+    private void listRequest(SelectionKey key, String path) throws IOException {
+        SocketChannel channel = ((SocketChannel) key.channel());
+        String message = "";
+        File directory = new File(path);
+        File[] files;
+        if (directory.exists()) {
+            if (directory.isFile()) {
+                files = new File[]{directory};
+            } else {
+                files = directory.listFiles();
+            }
+            message += files.length;
+            for (var file : files) {
+                message += " (";
+                message += file.getName() + " ";
+                message += file.isDirectory() ? 1 : 0;
+                message += ")";
+            }
+        } else {
+            message += -1;
+        }
+
+        Charset charset = Charset.forName("ISO-8859-1");
+
+        byte[] messageBytes = message.getBytes(charset);
+        ByteBuffer buffer = ByteBuffer.allocate(Math.max(messageBytes.length, BUFFER_SIZE));
+        buffer.put(messageBytes);
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            channel.write(buffer);
+        }
+        buffer.clear();
+        key.interestOps(key.interestOps() ^ SelectionKey.OP_WRITE);
     }
 }
